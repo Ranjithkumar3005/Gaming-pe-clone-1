aadc4b7a519e148d2cbb2ce8d0c4cd1d263c6d39
Hemanthyt <hemanth25052004@gmail.com>
2025-02-13 22:46:48 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className=" text-5xl text-blue-500 font-bold "> Hello world! </h1>
+      <button className=" text-blue bg-red-500 px-4 py-2">Byee</button>
+    </div>
+  );
+};
+
+export default App;
