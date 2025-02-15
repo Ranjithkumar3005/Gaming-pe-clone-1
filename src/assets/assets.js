56411b3d0dd281edcef231ea39bcee8e414a0d36
Hemanthyt <hemanth25052004@gmail.com>
2025-02-15 11:18:16 +0530

    Required assets added

---
+++ src/assets/assets.js
@@ -0,0 +1,33 @@
+import Tc1 from "../assets/Tc1.png";
+import Tc2 from "../assets/Tc2.png";
+import Tc3 from "../assets/Tc3.png";
+import Tc4 from "../assets/Tc4.png";
+import Tc5 from "../assets/Tc5.png";
+import PT1 from "../assets/PT1.svg";
+import PT2 from "../assets/PT3.svg";
+import PT3 from "../assets/PT2.svg";
+import Ether from "../assets/Ether.svg";
+import Bitcoin from "../assets/Bitcoin.svg";
+import Globe from "../assets/globe.mp4";
+import HandMobile from "../assets/api-image.svg";
+import CryptoCoin from "../assets/cryptocoin.png";
+import vision from "../assets/vision1.svg";
+import map from "../assets/map.svg";
+
+export const assets = {
+  Tc1,
+  Tc2,
+  Tc3,
+  Tc4,
+  Tc5,
+  PT1,
+  PT2,
+  PT3,
+  Ether,
+  Bitcoin,
+  Globe,
+  HandMobile,
+  CryptoCoin,
+  vision,
+  map,
+};
